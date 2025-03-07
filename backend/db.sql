0a529e5e340e50d0255e8938f01910b8117a4956
GABRIEL PEREIRA DOS SANTOS <05091921060@senacrs.edu.br>
2025-03-07 11:33:42 -0300

    atualizado

--- backend/db.sql
+++ backend/db.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE db_prova;
-USE db_prova;
+CREATE DATABASE estacionamento;
+USE estacionamento;
 
 CREATE TABLE users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE products (
+	
+CREATE TABLE cars (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL (10, 2) NOT NULL
+    placa VARCHAR(255) NOT NULL
 );
